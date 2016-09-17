8887804a4c498f4985d4ac787f566a6a8067b110
Paweł Buchowski <pawel.buchowski@refactory.net>
2016-09-17 02:56:26 +0200

    add fitbit lib

--- moveDevServer/views.py
+++ moveDevServer/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+class Home(TemplateView):
+    template_name = 'index.html'
+
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+        else:
+            pass
+        return self.render_to_response({})
