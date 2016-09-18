4511b8d9f384df54111d8b4b85cbd62428c1f32e
Paweł Buchowski <pawel.buchowski@refactory.net>
2016-09-18 07:32:16 +0200

    make it a game

--- game/models.py
+++ game/models.py
@@ -12,7 +12,7 @@ import json
 class PlayBoard(models.Model):
     currstate = models.CommaSeparatedIntegerField(max_length=1000, null=True, blank=True)  # the gameboard
     player1 = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='player1')
-    player2 = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='player2')
+    player2 = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='player2', null=True, blank=True)
     game_id = models.CharField(max_length=100)
     finished_at = models.DateTimeField(null=True, blank=True)
 
--- game/views.py
+++ game/views.py
@@ -1,38 +1,50 @@
 import json
-import random
-import string
 
 from django.contrib.auth.models import User
 from django.db.models import Q
 from django.http import HttpResponse
 from game.models import PlayBoard
 from django.views.decorators.csrf import csrf_exempt
-from django.db import IntegrityError, transaction
+from django.utils import timezone
+from django.db import transaction
 import random
-import string
 
 
 @csrf_exempt
 @transaction.atomic
 def index(request):
     if request.method == 'GET':
-        board, created = PlayBoard.objects.get_or_create(
-            player1=request.user,
+        boards = PlayBoard.objects.filter(
+            Q(player1=request.user) | Q(player2=request.user),
             game_id=0,
             finished_at=None
         )
+        created = True
+        if boards.count() == 1:
+            board = boards[0]
+            created = False
+        elif boards.count() > 1:
+            boards.delete()
+            created = True
         if created:
+            board = PlayBoard(player1=request.user, game_id=0, finished_at=None)
             # randomizing players positions on new boards
-            board.currstate = [[0 for i in range(0, 10)] for i in range(0, 10)]  # blank matrix
+            board.currstate = [[{'player_id': 0, 'value': 0} for i in range(0, 10)] for i in
+                               range(0, 10)]  # blank matrix
             row, col = random.randint(0, 9), random.randint(0, 9)
-            board.currstate[row][col] = board.player1.profile.credits
-            row, col = random.randint(0, 9), random.randint(0, 9)
+            board.currstate[row][col]['value'] = board.player1.profile.credits
+            board.currstate[row][col]['player_id'] = board.player1.id
+
             board.player2 = User.objects.filter(~Q(id=request.user.id))[
                 random.randint(0, User.objects.filter(~Q(id=request.user.id)).count() - 1)]
-            board.currstate[row][col] = board.player2.profile.credits
+            row, col = random.randint(0, 9), random.randint(0, 9)
+            board.currstate[row][col]['value'] = board.player2.profile.credits
+            board.currstate[row][col]['player_id'] = board.player2.id
+            board.currstate = json.dumps(board.currstate)
 
             board.save()
-        return HttpResponse(json.dumps({"gameState": board.currstate, "gameId": board.id}), content_type="application/json")
+        return HttpResponse(json.dumps({"gameState": board.currstate, "gameId": board.id, "owner": request.user.id}),
+                            content_type="application/json")
     else:
         return HttpResponse({})
 
@@ -48,14 +60,46 @@ def play(request):
 
         board = PlayBoard.objects.get(id=request.GET['id'])
         currstate = json.loads(board.currstate)
-        sender_value = currstate[sender_x][sender_y]
-        currstate[rcvr_x][rcvr_y] = sender_value / 2
-        currstate[sender_x][sender_y] = sender_value / 2
-        board.currstate = json.dumps(currstate)
+        print(int(currstate[sender_x][sender_y]['player_id']))
+        print(request.user.id)
+        print(int(currstate[sender_x][sender_y]['player_id']) == request.user.id)
         print(currstate)
-        print(rcvr_x, rcvr_y)
-        print(sender_x, sender_y)
-        board.save()
-        return HttpResponse(json.dumps({"gameState": board.currstate, "gameId": board.id}), content_type="application/json")
+        if int(currstate[sender_x][sender_y]['player_id']) == request.user.id:
+            sender_value = currstate[sender_x][sender_y]['value']
+            currstate[sender_x][sender_y]['value'] = sender_value / 2
+            if int(currstate[rcvr_x][rcvr_y]['player_id']) == request.user.id or int(
+                    currstate[rcvr_x][rcvr_y]['player_id']) == 0:
+                currstate[rcvr_x][rcvr_y]['value'] += + sender_value / 2
+                currstate[rcvr_x][rcvr_y]['player_id'] = request.user.id
+            else:
+                if sender_value > currstate[rcvr_x][rcvr_y]['value']:
+                    currstate[rcvr_x][rcvr_y]['player_id'] = request.user.id
+                currstate[rcvr_x][rcvr_y]['value'] = abs(currstate[rcvr_x][rcvr_y]['value'] - sender_value / 2)
+            board.currstate = json.dumps(currstate)
+            board.save()
+        return HttpResponse(json.dumps({"gameState": board.currstate, "gameId": board.id, "owner": request.user.id}),
+                            content_type="application/json")
     else:
         return HttpResponse(status=200)
+
+
+@csrf_exempt
+def update(request):
+    board = PlayBoard.objects.get(id=request.GET['id'])
+    currste = json.loads(board.currstate)
+    p1credits = 0
+    p2credits = 0
+    gameFinished = False
+    for row in currste:
+        p1credits += reduce(lambda aku, d: aku + d['value'],
+                            filter(lambda col: int(col['player_id']) == board.player1.id, row), 0)
+        p2credits += reduce(lambda aku, d: aku + d['value'],
+                            filter(lambda col: int(col['player_id']) == board.player2.id, row), 0)
+
+    if p2credits == 0:
+        gameFinished = True
+        board.finished_at = timezone.now()
+        board.save()
+    return HttpResponse(json.dumps(
+        {"gameState": board.currstate, "gameId": board.id, "owner": request.user.id, "gamefinished": gameFinished}),
+        content_type="application/json")
